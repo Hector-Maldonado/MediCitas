b2eb894783c8d3530143828c665464c7754228fb
Hector-Maldonado <maldonadohector54@gmail.com>
2025-10-09 17:19:32 -0600

    MediCitas - aplicación para la administración de citas médicas

--- src/main/java/com/medicitas/repository/AppointmentRepository.java
+++ src/main/java/com/medicitas/repository/AppointmentRepository.java
@@ -4,8 +4,6 @@ import com.medicitas.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
